1ff9a9465f1ddd8b22ee6e4e36e4c399919f3966
Maddie Zug <mzug@weddingwire.com>
2016-08-02 09:52:28 -0400

    Fixed code formatting

--- Sources/DatePickerField.swift
+++ Sources/DatePickerField.swift
@@ -10,16 +10,16 @@ import Foundation
 import UIKit
 
 /// UITextField with UIDatePicker as input method
-public class PickerField : TextField {
+public class DatePickerField : TextField {
     public var datePicker:UIDatePicker = UIDatePicker()
     
     public required init(validators: [ValidationRule], setup: (TextField -> Void)?) {
         super.init(validators: validators, setup: setup)
+        
         self.inputView = datePicker
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
--- Sources/PickerField.swift
+++ Sources/PickerField.swift
@@ -15,11 +15,11 @@ public class PickerField : TextField {
     
     public required init(validators: [ValidationRule], setup: (TextField -> Void)?) {
         super.init(validators: validators, setup: setup)
+        
         self.inputView = pickerView
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
